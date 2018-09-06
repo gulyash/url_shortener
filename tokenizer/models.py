0c6317b4a2594bf0cec845ef493cf006d336b397
Gulnara Ibragimova <igulnara6@gmail.com>
2018-09-06 23:10:05 +0500

    User FK added

--- tokenizer/models.py
+++ tokenizer/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -18,6 +19,16 @@ class Url(models.Model):
         verbose_name='Дата создания',
         default=timezone.now
     )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE
+    )
     redirect_count = models.IntegerField(
         verbose_name='Количество переходов по ссылке',
+        default=0
     )
+#
+# class View(models.Model):
+#     session_id = models.CharField(max_length=256, blank=False)
+#     timestamp = models.DateTimeField(auto_now_add=True, blank=False)
+#     url = models.ForeignKey(Url, blank=False)
