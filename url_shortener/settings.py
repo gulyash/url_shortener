f37d44479aaa1ba12bf3f0957f62ea078711b938
Gulnara Ibragimova <igulnara6@gmail.com>
2018-09-07 16:25:03 +0500

    Oops, you haven't seen that

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l44cl0+hp7=s236lc0tpt3+7@nbp(7$hocpv)v7-l77n3%hts8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
